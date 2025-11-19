65132bbdc5623fce0b3ec842346fbdd12672ec34
DiverbrasPaulinho <pauloedson0807@gmail.com>
2025-11-19 15:40:54 -0300

    Fix: ajusta next.config,mjs para produção

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Adicionando reactStrictMode para garantir que o React seja bem configurado
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +10,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  swcMinify: true,  // Habilita minificação com SWC
+  trailingSlash: true,  // Garante que as URLs terminem com barra
+  poweredByHeader: false,  // Opcional: desabilita o cabeçalho X-Powered-By para segurança
 }
 
 export default nextConfig
+
