35ee3ca3d881462dd51bf1acbff9378024cd30bd
Paulo Edson <112034255+DiverbrasPaulinho@users.noreply.github.com>
2025-11-19 15:13:25 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,11 @@ import type { Metadata } from "next"
 import { Mona_Sans as FontSans } from "next/font/google"
 import { Poppins } from "next/font/google"
 import { cn } from "@/lib/utils"
-
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/components/auth-provider"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
-
 import "./globals.css"
 
 const fontSans = FontSans({
