194f33a353197758f416a49846179cab40ce51d6
DiverbrasPaulinho <pauloedson0807@gmail.com>
2025-11-19 13:24:26 -0300

    fix: clean neon.css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Mona_Sans as FontSans } from "next/font/google"
-import localFont from "next/font/local"
+import { Poppins } from "next/font/google"
 import { cn } from "@/lib/utils"
+
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/components/auth-provider"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
-import "./globals.css"
-import { Poppins } from "next/font/google"
-import "app\neon.css"
 
+import "./globals.css"
+import "./neon.css" // <-- 100% correto
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,25 +20,37 @@ const fontSans = FontSans({
 
 const fontHeading = Poppins({
   subsets: ["latin"],
-  weight: ["600","700"],
+  weight: ["600", "700"],
   variable: "--font-heading",
   display: "swap",
 })
 
 export const metadata: Metadata = {
   title: "Plush For You Stream",
-  description: "Pegue o pelúcia online e receba na sua casa ou no endereço que quiser, como presente",
+  description:
+    "Pegue o pelúcia online e receba na sua casa ou no endereço que quiser, como presente",
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="pt" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable, fontHeading.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           <AuthProvider>
             <div className="relative flex min-h-screen flex-col">
               <Header />
